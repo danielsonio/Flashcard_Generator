fb8bd348ca69a2f2079e951fe5c5a05ca7cbd1d6
Daniel Walden <Ariel@Daniels-MacBook-Pro.local>
2017-05-23 15:00:59 -0500

    changed basic and cloze to constructor functions

--- flashcards.js
+++ flashcards.js
@@ -1,5 +1,4 @@
 
-
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
@@ -30,93 +29,101 @@ var start = function() {
         }
     ]).then(function (answers) {
         if(answers.choice === "Basic") {
-          basic();
-
-        } else {
-          cloze();
-        }
-    });
-}
-
-var cloze = function() {
-  connection.query("SELECT * FROM cloze", function(err, res){
-    if(err) throw err;
-    if (count>res.length-1){
-      count = 0;
-    }
-    var str = res[count].choices;
-    var options = str.split(",");
-    // console.log(options);
-    inquirer.prompt([
-      {
-        type: "list",
-        message: res[count].partial,
-        choices: options,
-        name: "choice"
-      }
-    ]).then(function(answers){
-      if(answers.choice === res[count].cloze){
-        console.log("Correct!");
-        console.log(res[count].full);
-        setTimeout(restart2,3000);
-      } else {
-        console.log("Incorrect!");
-        console.log(res[count].full);
-        setTimeout(restart2,3000);
-      }
-    });
-  });
-}
-
-
-
-var basic = function() {
-  inquirer.prompt([
-      {
-        type: "list",
-        message: "Read or Make a card?",
-        choices: ["Read","Make"],
-        name: "choice"
-      }
-  ]).then(function (answers) {
-      if(answers.choice === "Read") {
-        connection.query("SELECT * FROM basic", function(err, res){
-          if (err) throw err;
-          if (count>res.length-1){
-            count = 0;
-          }
-          console.log(res[count].question);
-          var answer = function(){console.log(res[count].answer)};
-          setTimeout(answer,1500);
-          setTimeout(restart,3000);
-        })
-      } else {
-        inquirer.prompt([
+          inquirer.prompt([
             {
-              type: "input",
-              message: "What question would you like to ask?",
-              name: "question"
-            },
-            {
-              type: "input",
-              message: "What is the answer?",
-              name: "answer"
+              type: "list",
+              message: "Read or Make a card?",
+              choices: ["Read","Make"],
+              name: "choice"
             }
-        ]).then(function (answers) {
-          connection.query("INSERT INTO basic SET ?", {question: answers.question, answer: answers.answer},
-            function(err, res) {
-            if (err) throw err;
-            console.log("Your question: "+ answers.question);
-            console.log("Answer: "+ answers.answer);
-            console.log("Added successfully!");
-            setTimeout(restart,1500);
-            });
+          ]).then(function(answers) {
+            if(answers.choice === "Read"){
+              var newBasic = new Basic();
+              newBasic.read();
+            } else {
+              var newBasic = new Basic();
+              newBasic.write();
+            }
         });
+      } else {
+        var newCloze = new Cloze();
+        newCloze.read();
       }
   });
 }
 
 
+var Cloze = function() {
+  this.read = function() {
+    connection.query("SELECT * FROM cloze", function(err, res){
+      if(err) throw err;
+      if (count>res.length-1){
+        count = 0;
+      }
+      var str = res[count].choices;
+      var options = str.split(",");
+      // console.log(options);
+      inquirer.prompt([
+        {
+          type: "list",
+          message: res[count].partial,
+          choices: options,
+          name: "choice"
+        }
+      ]).then(function(answers){
+        if(answers.choice === res[count].cloze){
+          console.log("Correct!");
+          console.log(res[count].full);
+          setTimeout(restart,1500);
+        } else {
+          console.log("Incorrect!");
+          console.log(res[count].full);
+          setTimeout(restart,1500);
+        }
+      });
+    });
+  }
+}
+
+
+
+var Basic = function() {
+  this.read = function() {
+    connection.query("SELECT * FROM basic", function(err, res){
+      if (err) throw err;
+      if (count>res.length-1){
+        count = 0;
+      }
+      console.log(res[count].question);
+      var answer = function(){console.log(res[count].answer)};
+      setTimeout(answer,1500);
+      setTimeout(restart,3000);
+    });
+  },
+  this.write = function() {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "What question would you like to ask?",
+          name: "question"
+        },
+        {
+          type: "input",
+          message: "What is the answer?",
+          name: "answer"
+        }
+    ]).then(function (answers) {
+      connection.query("INSERT INTO basic SET ?", {question: answers.question, answer: answers.answer},
+        function(err, res) {
+        if (err) throw err;
+        console.log("Your question: "+ answers.question);
+        console.log("Answer: "+ answers.answer);
+        console.log("Added successfully!");
+        setTimeout(restart,1500);
+        });
+    });
+  }
+}
 
 
 
@@ -127,34 +134,12 @@ var restart = function() {
       {
         type: "list",
         message: "Would you like another card?",
-        choices: ["Yes","Return to main menu","Quit session"],
+        choices: ["Yes","Quit session"],
         name: "choice"
       }
   ]).then(function (answers) {
     if(answers.choice === "Yes"){
-      basic();
       count++
-    } else if(answers.choice === "Return to main menu") {
-      start();
-    } else {
-      connection.end();
-    }
-  });
-}
-
-var restart2 = function() {
-     inquirer.prompt([
-      {
-        type: "list",
-        message: "Would you like another card?",
-        choices: ["Yes","Return to main menu","Quit session"],
-        name: "choice"
-      }
-  ]).then(function (answers) {
-    if(answers.choice === "Yes"){
-      cloze();
-      count++
-    } else if(answers.choice === "Return to main menu") {
       start();
     } else {
       connection.end();
